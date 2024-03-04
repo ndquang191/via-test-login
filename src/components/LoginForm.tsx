37e322a1c5503e28fd0b7ec8ce0bd8535de49bfe
Quang Nguyen <ndquang191@gmail.com>
2024-03-04 13:51:06 +0700

    before i Shadcn

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,16 +1,51 @@
 "use client";
 
 import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-type LoginField = {
-	email: String;
-	password: string;
-};
+const schema = z.object({
+	email:
+		z.string().email() || z.string().regex(/^(0|\+84)(3[2-9]|5[6|8|9]|7[0|6-9]|8[1-6|8-9]|9\d)\d{7}$/),
+	password: z.string(),
+});
+
+type LoginField = z.infer<typeof schema>;
+
 const LoginForm = () => {
-	const { register, handleSubmit } = useForm<LoginField>();
+	const {
+		register,
+		handleSubmit,
+		formState: { errors, isSubmitting },
+	} = useForm<LoginField>({
+		resolver: zodResolver(schema),
+	});
 
-	const onSubmit = (data: any) => {
+	const onSubmit = async (data: any) => {
 		console.log(data);
+
+		const result = await fetch("https://dev-fe-exam.viajsc.com/ExamUser/login", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ username: data.email, password: data.password }),
+		})
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error("Login failed");
+				}
+				return response.json();
+			})
+			.then((data) => {
+				console.log("Login successful:", data);
+				return data;
+			})
+			.catch((error) => {
+				// Handle errors
+				console.error("Error logging in:", error.message);
+				throw error;
+			});
 	};
 	return (
 		<div className="bg-white p-7  rounded ">
@@ -22,12 +57,7 @@ const LoginForm = () => {
 						className="border-2 border-input p-2 rounded w-full"
 						type="text"
 						placeholder="Nhập số điện thoại hoặc Email..."
-						{...register("email", {
-							required: true,
-							pattern:
-								/^(0|\+84)(3[2-9]|5[6|8|9]|7[0|6-9]|8[1-6|8-9]|9\d)\d{7}$/ ||
-								/\S+@\S+/,
-						})}
+						{...register("email")}
 					/>
 				</div>
 
@@ -44,6 +74,7 @@ const LoginForm = () => {
 				</div>
 
 				<button
+					disabled={isSubmitting}
 					type="submit"
 					className="block w-[120px] h-[45px] text-center bg-primary text-white rounded relative top-[22px]"
 				>
